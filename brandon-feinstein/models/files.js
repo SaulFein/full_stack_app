0d18e7aa25bce3983c858554ee3ad97c693444ee
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-03-23 18:58:29 -0700

    changed models and added authentication

--- brandon-feinstein/models/files.js
+++ brandon-feinstein/models/files.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const mercedesSchema = new mongoose.Schema({
+const fileSchema = new mongoose.Schema({
   name: String,
-  raceWins: Number,
-  team: {type: String, default: 'Mercedes'}
+  content: String
 });
 
-module.exports = mongoose.model('Mercedes', mercedesSchema);
+module.exports = mongoose.model('Files', fileSchema);
 
 //curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:3000/api/login
 // '{"name":"Kimi Raikkonen", "raceWins":20}' '{"name":"Sebastian Vettel", "raceWins":42}'
--- brandon-feinstein/server.js
+++ brandon-feinstein/server.js
@@ -3,99 +3,133 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 let mongoose = require('mongoose');
-let Ferrari = require(__dirname + '/models/ferrari-f1-drivers');
-let Mercedes = require(__dirname + '/models/mercedes-f1-drivers');
+let Users = require(__dirname + '/models/users');
+// let Files = require(__dirname + '/models/files');
+let auth = require('./lib/authenticate');
 
 
-let DB_PORT = process.env.MONGOLAB_URI || 'mongodb://localhost/db'
+var PORT = process.env.PORT || 3000;
+
+let DB_PORT = process.env.MONGOLAB_URI || 'mongodb://localhost/db';
 mongoose.connect(DB_PORT);
 
 app.use(bodyParser.json());
 
-app.get('/ferrari-drivers', (req, res) => {
-  Ferrari.find({}, (err, fdrivers) => {
-    res.json({data: fdrivers});
-  })
-})
-
-app.get('/ferrari-drivers/:id', (req, res) => {
-  Ferrari.findById(req.params.id, (err, fdriver) => {
-    res.json(fdriver);
-  })
-})
-
-app.post('/ferrari-drivers', (req, res) => {
-  var newFdriver = new Ferrari(req.body);
-  newFdriver.save((err, fdriver) => {
-    res.json(fdriver);
-  })
-})
-
-app.put('/ferrari-drivers/:id', (req, res) => {
-  Ferrari.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, fdriver) => {
-    res.json(fdriver);
-    console.log(fdriver);
-  })
-})
-
-app.delete('/ferrari-drivers/:id', (req, res) => {
-  Ferrari.findById(req.params.id, (err, fdriver) => {
-    if (err) res.json({err: 'errors'})
-    fdriver.remove((err, fdriver) => {
-      res.json({message: 'Ferrari driver removed'});
-    })
-  })
-})
-
-app.get('/ferr-mostwins', (req, res) => {
-  Ferrari.find().sort({raceWins: -1}).limit(1).exec((err, fdrivers) => {
-    // console.log(fdrivers);
-    res.json({data: fdrivers});
+app.get('/users', (req, res) => {
+  Users.find({}, (err, users) => {
+    res.json({data: users});
   });
 });
 
-app.get('/mercedes-drivers', (req, res) => {
-  Mercedes.find({}, (err, mdrivers) => {
-    res.json({data: mdrivers});
-  })
-})
-
-app.get('/mercedes-drivers/:id', (req, res) => {
-  Mercedes.findById(req.params.id, (err, mdriver) => {
-    res.json(mdriver);
-  })
-})
-
-app.post('/mercedes-drivers', (req, res) => {
-  var newMdriver = new Mercedes(req.body);
-  newMdriver.save((err, mdriver) => {
-    res.json(mdriver);
-  })
-})
-
-app.put('/mercedes-drivers/:id', (req, res) => {
-  Mercedes.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, mdriver) => {
-    if (err) return res.send(err);
-    res.json(mdriver);
-  })
-})
-
-app.delete('/mercedes-drivers/:id', (req, res) => {
-  Mercedes.findById(req.params.id, (err, mdriver) => {
-    if (err) res.json({err: 'errors'})
-    mdriver.remove((err, mdriver) => {
-      res.json({message: 'Mercedes driver removed'});
-    })
-  })
-})
-
-app.get('/merc-mostwins', (req, res) => {
-  Mercedes.find().sort({raceWins: -1}).limit(1).exec((err, mdrivers) => {
-    // console.log(mdrivers);
-    res.json({data: mdrivers});
+app.get('/users/:id', (req, res) => {
+  Users.findById(req.params.id, (err, user) => {
+    res.json(user);
   });
 });
 
-app.listen(3000, () => {
-  console.log('server started')
-})
+app.post('/users', (req, res) => { //create new user
+  var newUser = new Users(req.body);
+  console.log(newUser);
+  newUser.save((err, user) => {
+    if (err) res.json({err: 'errors'});
+    res.json(user);
+  });
+});
+
+//curl -H "Content-Type: application/json" -X POST -d '{"name":"user1","password":"123"}' http://localhost:3000/users
+
+
+app.put('/users/:id', (req, res) => {
+  Users.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, user) => {
+    res.json(user);
+    console.log(user);
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+  Users.findById(req.params.id, (err, user) => {
+    if (err) res.json({err: 'errors'});
+    user.remove((err, user) => {
+      res.json({message: user + 'User removed'});
+    });
+  });
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.headers.authorization);
+  let authorizationArray = req.headers.authorization.split(' ');
+  let method = authorizationArray[0];
+  let base64ed = authorizationArray[1];
+  let authArray = new Buffer(base64ed, 'base64').toString().split(':');
+  let name = authArray[0];
+  let password = authArray[1];
+  console.log(authArray);
+  console.log(method);
+  console.log(name);
+  // parse based on basic or whatever method
+  Users.findOne({name: name}, (err, user) => {
+    console.log('in user find');
+    if (err) res.json({err: 'errors'});
+    let valid = user.compareHash(password);
+    if (!valid) {
+      return res.json({status: 'failure'});
+    }
+      // generate and return the token
+    res.json({token: user.generateToken()});
+  });
+});
+
+app.get('/login', auth);
+
+// app.get('/files', (req, res) => {
+//   Files.find({}, (err, files) => {
+//     res.json({data: files});
+//   });
+// });
+//
+// app.get('/files/:id', (req, res) => {
+//   Files.findById(req.params.id, (err, file) => {
+//     res.json(file);
+//   });
+// });
+//
+// app.post('/files', (req, res) => {
+//   var newFile = new Files(req.body);
+//   newFile.save((err, file) => {
+//     res.json(file);
+//   });
+// });
+//
+// app.put('/files/:id', (req, res) => {
+//   Files.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, file) => {
+//     if (err) return res.send(err);
+//     res.json(file);
+//   });
+// });
+//
+// app.delete('/files/:id', (req, res) => {
+//   Files.findById(req.params.id, (err, file) => {
+//     if (err) res.json({err: 'errors'});
+//     file.remove((err, file) => {
+//       res.json({message: 'File removed'});
+//     });
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log('server started');
+});
+
+// app.get('/ferr-mostwins', (req, res) => {
+//   Ferrari.find().sort({raceWins: -1}).limit(1).exec((err, fdrivers) => {
+//     // console.log(fdrivers);
+//     res.json({data: fdrivers});
+//   });
+// });
+
+// app.get('/merc-mostwins', (req, res) => {
+//   Mercedes.find().sort({raceWins: -1}).limit(1).exec((err, mdrivers) => {
+//     // console.log(mdrivers);
+//     res.json({data: mdrivers});
+//   });
+// });
